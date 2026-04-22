fe95fa130128c802d3f0e140a79acdf78c01873c
7Tiamatr/x_ <159614599+Synthxyl704@users.noreply.github.com>
2026-04-22 23:01:03 +0530

    comment out redundant code

--- src/topology.py
+++ src/topology.py
@@ -62,67 +62,67 @@ def getCacheTopology() -> Dict[str, Dict[int, List[int]]]:
     cacheData: Dict = {}; # type[Dict] = {};
 
     # L1I[nstructions], L1D[ata], L2, L3 per level
-    for cacheLevel in ["1", "2", "3"]:
-        cacheData[cacheLevel] = {};
+    # for cacheLevel in ["1", "2", "3"]:
+    #     cacheData[cacheLevel] = {};
 
-    for CPU_id in range(CPU_count):
-        # construct a file path string for the current CPU ID 
-        # and then join it with cache (Str) to create a cacheBase path
-        # if that path does not exist, we skip promptly
+    # for CPU_id in range(CPU_count):
+    #     # construct a file path string for the current CPU ID 
+    #     # and then join it with cache (Str) to create a cacheBase path
+    #     # if that path does not exist, we skip promptly
 
-        # os.path.join(path, *paths)
-        # paths = ["devices", "system", "cpu"]
-        # os.path.join("/sys", *paths)
-        CPU_path: str = f"/sys/devices/system/cpu/cpu{CPU_id}";
-        cacheBase:str = os.path.join(CPU_path, "cache");
+    #     # os.path.join(path, *paths)
+    #     # paths = ["devices", "system", "cpu"]
+    #     # os.path.join("/sys", *paths)
+    #     CPU_path: str = f"/sys/devices/system/cpu/cpu{CPU_id}";
+    #     cacheBase:str = os.path.join(CPU_path, "cache");
         
-        if (not os.path.exists(cacheBase)):
-            continue;
+    #     if (not os.path.exists(cacheBase)):
+    #         continue;
         
-        for cacheIndex in ["index0", "index1", "index2", "index3"]:
-            indexPath: str = os.path.join(cacheBase, cacheIndex);
-            # /sys/.../cpu2/cache/index[0]
+    #     for cacheIndex in ["index0", "index1", "index2", "index3"]:
+    #         indexPath: str = os.path.join(cacheBase, cacheIndex);
+    #         # /sys/.../cpu2/cache/index[0]
 
-            if not os.path.exists(indexPath):
-                continue;
+    #         if not os.path.exists(indexPath):
+    #             continue;
 
-            try: # extract cache metadata
-                cacheLevel: str = open(os.path.join(indexPath, "level")).read().strip(); # 1, 2, 3 
-                cacheTypeKey: str = open(os.path.join(indexPath, "type")).read().strip();# Instruction (L1I), Data (L1D), Unified
-                sharedCPUmap: str = (open(os.path.join(indexPath, "shared_cpu_map")).read().strip()); # bitmask of cpus sharing this cache
-            except:
-                continue;
+    #         try: # extract cache metadata
+    #             cacheLevel: str = open(os.path.join(indexPath, "level")).read().strip(); # 1, 2, 3 
+    #             cacheTypeKey: str = open(os.path.join(indexPath, "type")).read().strip();# Instruction (L1I), Data (L1D), Unified
+    #             sharedCPUmap: str = (open(os.path.join(indexPath, "shared_cpu_map")).read().strip()); # bitmask of cpus sharing this cache
+    #         except:
+    #             continue;
 
-            if (not cacheLevel):
-                continue;
+    #         if (not cacheLevel):
+    #             continue;
 
-            CPU_list: List[int] = parseCPUMask(sharedCPUmap);
+    #         CPU_list: List[int] = parseCPUMask(sharedCPUmap);
 
-            # build key: L1I, L1D, L2, L3
-            if cacheLevel == "1":
-                if cacheTypeKey == "Instruction":
-                    cacheTypeKey = "L1I";
-                elif cacheTypeKey == "Data":
-                    cacheTypeKey = "L1D";
-                else:
-                    cacheTypeKey = "L1";
-            else:
-                cacheTypeKey = f"L{cacheLevel}";
+    #         # build key: L1I, L1D, L2, L3
+    #         if cacheLevel == "1":
+    #             if cacheTypeKey == "Instruction":
+    #                 cacheTypeKey = "L1I";
+    #             elif cacheTypeKey == "Data":
+    #                 cacheTypeKey = "L1D";
+    #             else:
+    #                 cacheTypeKey = "L1";
+    #         else:
+    #             cacheTypeKey = f"L{cacheLevel}";
 
-            # group by exact core set 
-            found: bool = False;
-            for existing_key, existing_cores in cacheData[cacheLevel].items():
-            # cacheData[cacheLevel="2"] = {
-                # 0: [0,1],
-                # 1: [2,3]...
-            # }
-                if existing_cores == CPU_list:
-                    found = True;
-                    break;
+    #         # group by exact core set 
+    #         found: bool = False;
+    #         for existing_key, existing_cores in cacheData[cacheLevel].items():
+    #         # cacheData[cacheLevel="2"] = {
+    #             # 0: [0,1],
+    #             # 1: [2,3]...
+    #         # }
+    #             if existing_cores == CPU_list:
+    #                 found = True;
+    #                 break;
 
-            if not found:
-                cacheData[cacheLevel][len(cacheData[cacheLevel])] = CPU_list; # add new shared cache CPU(s) 
-                                                                              # using intance such as 2: [4, 5]
+    #         if not found:
+    #             cacheData[cacheLevel][len(cacheData[cacheLevel])] = CPU_list; # add new shared cache CPU(s) 
+    #                                                                           # using intance such as 2: [4, 5]
 
     mapOfCacheTopologyRESULT: Dict = {};
     cacheLevelMap: Dict[str, str] = {"1": "L1", "2": "L2", "3": "L3"};
